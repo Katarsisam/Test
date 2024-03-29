cfa5e16ffa98581464f2baf268c540d7a05eb82f
Katarsisam <krakik20032014@gmail.com>
2024-03-30 03:17:25 +0700

    Комаров TRY

---
+++ srcKA/ConsolePR.cpp
@@ -0,0 +1,18 @@
+﻿
+
+#include <iostream>
+#include <stdio.h>
+
+int main()
+{
+    setlocale(LC_ALL, "RUS");
+    int a, b, S, P;
+    double d;
+    scanf_s("%d %d", &a, &b);
+    P = (a + b) * 2;
+    S = a * b;
+    d = sqrt(pow(a, 2) * pow(b, 2));
+    printf("Площадь: %d, Периметр: %d, Диагональ: %.2lf", S, P, d);
+}
+
+
