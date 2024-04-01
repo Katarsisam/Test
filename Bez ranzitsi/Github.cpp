14da11fad631f2003f61ecd00977e36474eec061
Slesh <163682634+Slesh225@users.noreply.github.com>
2024-04-01 10:14:02 +0700

    Update Github.cpp

--- Bez ranzitsi/Github.cpp
+++ Bez ranzitsi/Github.cpp
@@ -1,15 +1,20 @@
-﻿#include <iostream>
+﻿
+
+#include <iostream>
 #include <stdio.h>
 
-int main() {
-	;
+int main(){
 	int a = 1;
-	int b = 2; //Меньшее основание
-	int c = 3; //Как и a - боковая сторона
-	int d = 4; //Большее основание
-	int h = 3; //высота
+	int b = 2;
+	int c = 3;
+	int d = 4;
+	int h = 3;
 	int P = a + b + c + d;
 	int S = (b + d) / h;
 	int l = (b + d) / 2;
-	printf("%d,%d,%d", P, S, l);
+	if (h > (a + c))
+		printf("Логическая ошибка");
+	else
+		printf("%d,%d,%d", P, S, l);
+		
 }
