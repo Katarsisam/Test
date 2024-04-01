115c523f9befb519d46843e7875c3012b98e2c79
Slesh <163682634+Slesh225@users.noreply.github.com>
2024-04-01 10:20:56 +0700

    Update Github.cpp
    Hello world

--- Bez ranzitsi/Github.cpp
+++ Bez ranzitsi/Github.cpp
@@ -9,12 +9,13 @@ int main(){
 	int c = 3;
 	int d = 4;
 	int h = 3;
-	int P = a + b + c + d;
-	int S = (b + d) / h;
-	int l = (b + d) / 2;
 	if (h > (a + c))
 		printf("Логическая ошибка");
 	else
 		printf("%d,%d,%d", P, S, l);
 		
+	int P = a + b + c + d;
+	int S = (b + d) / h;
+	int l = (b + d) / 2;
+	
 }
